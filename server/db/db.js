2213660a97fb186a556d71b09959390a7aab2a5e
Doug Miller <Djmiller1717@gmail.com>
2020-08-31 22:19:05 -0400

    added db url

--- server/db/db.js
+++ server/db/db.js
@@ -1,10 +1,6 @@
 const Sequelize = require("sequelize")
-const db= new Sequelize('postgres://localhost/arsenal', {
+const db= new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/arsenal', {
     logging: false
 });
-// if deployed
-// const acme = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/acme', {
-//     logging: false
-// });
 
 module.exports = db
